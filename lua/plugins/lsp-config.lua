2cd39d3160a39c000367e90358d6d11b1f85d50a
Ahmad Gharib <12767433+aygharib@users.noreply.github.com>
2023-12-29 17:05:38 -0500

    add lua LSP

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,29 @@
+return {
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = { "lua_ls", "clangd" }
+            })
+        end
+    },
+    {
+        "neovim/nvim-lspconfig",
+        -- Setup lua_ls
+        config = function()
+            local lspconfig = require("lspconfig")
+            lspconfig.lua_ls.setup({})
+            vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, {})
+            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+            vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+            vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
+            vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, {})
+        end
+    }
+}
