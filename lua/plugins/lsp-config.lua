0fe1048e78ed681c374d1ea98f26dbaccb61c077
Ahmad Gharib <12767433+aygharib@users.noreply.github.com>
2023-12-29 17:07:12 -0500

    add code-actions keybind to lspconfig

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -24,6 +24,7 @@ return {
             vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
             vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
             vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, {})
+            vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
         end
     }
 }
