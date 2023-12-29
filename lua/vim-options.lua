cf1a674544472648629d9b1f3d32ea15061fcc01
Ahmad Gharib <12767433+aygharib@users.noreply.github.com>
2023-12-29 16:25:49 -0500

    modularize config - separate plugins and vim options from main init.lua

---
+++ lua/vim-options.lua
@@ -0,0 +1,10 @@
+-- Tab gets converted into spaces
+-- Sets the default spacing to 4 spaces
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
+
+vim.g.mapleader = " "
+
+
